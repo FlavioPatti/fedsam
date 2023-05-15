5966a8b781bd45fee5f0835ad2a1e19b65aef6c4
GIUSEPPE PELLEGRINO s303999 <s303999@studenti.polito.it>
2023-05-15 11:15:28 +0200

    .

--- paper_experiments/cifar100.sh
+++ paper_experiments/cifar100.sh
@@ -9,7 +9,7 @@ declare -a alphas=("1000") #1000 , 0.5, 0
 function run_fedavg() {
   echo "############################################## Running FedAvg ##############################################"
   alpha="$1"
-  python main.py -dataset cifar100 --num-rounds 1000 --eval-every 100 --batch-size 512 --num-epochs 5 --clients-per-round 10 -model vit_small -lr 0.1 --weight-decay 0.0001 -device cuda:0 -algorithm fedopt --server-lr 1 --server-opt sgd --num-workers 0 --where-loading init -alpha ${alpha}
+  python main.py -dataset cifar100 --num-rounds 5000 --eval-every 100 --batch-size 512 --num-epochs 1 --clients-per-round 10 -model vit_small -lr 0.1 --weight-decay 0 -device cuda:0 -algorithm fedopt --server-lr 1 --server-opt sgd --num-workers 0 --where-loading init -alpha ${alpha}
 }
 #num rounds => 1000
 
